e9ea70ff36884fad64497a54f526a5fb7f226bb3
Sibina Rafi <sibinazzkabeer@gmail.com>
2025-07-29 10:39:28 +0530

    Finished Pharmacist

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const pharmacistRoutes = require('./routes/pharmacistRoutes');
+
+// Connect DB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/pharmacist', pharmacistRoutes);
+
+// Hello World Root (for basic testing)
+app.get('/', (req, res) => {
+  res.send('Clinical Management System Backend Running!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
