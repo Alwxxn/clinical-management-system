70a13d7ab293afed5cdc132e0a8b7c84413509d6
Alwxxn <allwynjoseph09@gmail.com>
2025-07-29 11:13:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,4 +2,19 @@
 
 const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/clinical_management', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // You can add other options here
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
 
