5410e59332fe6c1b52ddce602c6ab06146aff97d
Allwyn Joseph.S.J. <allwynjoseph09@gmail.com>
2025-07-29 14:19:05 +0530

    Without Admin

--- validators/adminValidators.js
+++
@@ -1,9 +0,0 @@
-// validators/adminValidators.js
-
-const { body } = require('express-validator');
-
-exports.adminValidators = [
-  body('username').notEmpty().withMessage('Username is required'),
-  body('email').isEmail().withMessage('Valid email is required'),
-  body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
-];
